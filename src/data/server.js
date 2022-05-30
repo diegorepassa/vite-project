9b306a1f1e83962e67caa77cd5d4007f7e09cd3b
diego.ferreira@repassa.com.br <diaugustdev@gmail.com>
2022-05-30 17:46:07 -0300

    bodyParser

--- src/data/server.js
+++ src/data/server.js
@@ -1,5 +1,8 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 console.log('Server is running');
 
@@ -9,4 +12,9 @@ app.listen(8080, () => {
 
 app.get('/', (req, res) => {
     res.sendFile('index.html', { root: '.' })
+})
+
+app.post('/depoimentos', (req, res) => {
+    console.log(req.body);
+    res.send('ok');
 })
