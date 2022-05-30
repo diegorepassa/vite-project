b449095a6e8350c562de35955967b000d1c805ab
diego.ferreira@repassa.com.br <diaugustdev@gmail.com>
2022-05-30 19:21:24 -0300

    remove dependencies

--- src/data/server.js
+++ src/data/server.js
@@ -1,18 +1,17 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import { MongoClient } from 'mongodb';
+import { MongoClient, ServerApiVersion } from 'mongodb';
 
-const uri = 'mongodb+srv://admin:teste123@crud.hbljf.mongodb.net/?retryWrites=true&w=majority';
 const app = express();
 
+const uri = "mongodb+srv://admin:teste123@crud.hbljf.mongodb.net/?retryWrites=true&w=majority";
+
 MongoClient.connect(uri, (err, db) => {
-    if (err) return console.error(err)
-    console.log('Connected to MongoDB')
-})
+  if (err) return console.error(err);
+  console.log('Connected to MongoDB');
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
-console.log('Server is running');
 
 app.listen(8080, () => {
     console.log('listening on port 8080');
