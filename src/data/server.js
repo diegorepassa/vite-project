8e40f0bb24e9412a603a871483d697345a70418f
diego.ferreira@repassa.com.br <diaugustdev@gmail.com>
2022-05-30 22:40:14 -0300

    server setup

--- src/data/server.js
+++ src/data/server.js
@@ -1,27 +1,48 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import { MongoClient } from 'mongodb';
+import express from "express";
+import bodyParser from "body-parser";
+import { MongoClient } from "mongodb";
 
 const app = express();
+const uri =
+  "mongodb+srv://admin:123@cluster0.2hxqm.mongodb.net/?retryWrites=true&w=majority";
 
-const uri = "mongodb+srv://admin:123@cluster0.2hxqm.mongodb.net/?retryWrites=true&w=majority";
-
-MongoClient.connect(uri, (err, db) => {
-  if (err) return console.error(err);
-  console.log('Connected to MongoDB');
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: "." });
 });
 
+// listen to 8080 port
+app.listen(8080, () => {
+  console.log("Server listening on port 8080");
+});
+
+// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(8080, () => {
-    console.log('listening on port 8080');
-})
+MongoClient.connect(uri)
+  .then((client) => {
+    console.log("Connected to MongoDB"); // message to connect at MongoDB
+    const db = client.db("start-wars-depoimentos"); // database name (a room)
+    const depoimentosCollection = db.collection("depoimentos"); // collection name (boxes in a room)
 
-app.get('/', (req, res) => {
-    res.sendFile('index.html', { root: '.' })
-})
+    // post request to save data in database (collection)
+    app.post("/depoimentos", (req, res) => {
+      depoimentosCollection
+        .insertOne(req.body)
+        .then((result) => {
+          console.log("Depoimento inserido no banco com sucesso");
+          res.redirect("/");
+        })
+        .catch((err) => console.error(err));
+    });
 
-app.post('/depoimentos', (req, res) => {
-    console.log(req.body);
-    res.send(req.body);
-})
+    app.get("/", (req, res) => {
+      depoimentosCollection
+        .find()
+        .toArray()
+        .then(results => {
+          console.log(results);
+        })
+        .catch(error => console.error(error));
+    });
+  })
+  .catch(console.error);
